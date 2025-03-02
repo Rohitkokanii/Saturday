29a02659c426830a4e963dd9ec771dc67f146385
Rohit Kokani <148274186+Rohitkokanii@users.noreply.github.com>
2025-03-03 00:14:24 +0530

    3march

--- Q11.c
+++ Q11.c
@@ -3,22 +3,13 @@
 int main() {
 
 	for (int i = 1; i <= 5; i++) {
+		for (int k = 1; k < i; k++) {
+			printf(" ");
+		}
 		for (int j = 1; j <= 5; j++) {
-			if (i == 2 && j <= 1) {
-				printf(" ");
-			}
-			else if (i == 3 && j <= 1) {
-				printf("  ");
-			}
-			else if (i == 4 && j <= 1) {
-				printf("   ");
-			}
-			else if (i == 5 && j <= 1) {
-				printf("    ");
-			}
 			printf("*");
 		}
-		printf("\n");
+		printf("\n");//print * in next line
 	}
 	return 0;
 }
